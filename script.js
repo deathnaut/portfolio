47266a01c7014a30d01bf44274ca481f311fd088
Karla Moreno <moreno_karla@live.com>
2017-11-20 00:50:06 -0600

    portfolio 40% done: animations&fun need work

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  console.log("javascript connected");
+  $(".main").addClass("load");
+  $(".projects").addClass("load");
+
+  $('#kollaboscope').click(function(){
+    $('#kollaboscope').animate();
+    console.log('a message should appear...');
+    $('#button-msg').show("slow");
+    console.log('redirecting...');
+    setTimeout(function () {
+      window.location.href = "https://kollaboscope-183217.firebaseapp.com/";
+    }, 2000);
+  });
+
+});
