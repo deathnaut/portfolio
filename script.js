a9003cd49aa9d4c2452b600b81fe71fec83fd578
Karla Moreno <moreno_karla@live.com>
2017-11-20 20:53:54 -0600

    got project sliding animation working

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
   console.log("javascript connected");
-  $(".main").addClass("load");
-  $(".projects").addClass("load");
+  $('.main').addClass('load');
+  $('.projects').addClass('load');
+
+  $('.project').click(function(){
+    var $this = $(this);
+    var showing = $this.css('min-height');
+    if (showing == '100%'){
+      $this.animate({ minHeight: '0%' }, 500);
+    } else {
+      $this.animate({ minHeight: '100%' }, 500);
+    }
+  });
 
   $('#kollaboscope').click(function(){
     $('#kollaboscope').animate();
-    console.log('a message should appear...');
+    console.log("a message should appear...");
     $('#button-msg').show("slow");
     console.log('redirecting...');
     setTimeout(function () {
@@ -15,9 +25,9 @@ $(document).ready(function () {
 
   $('#nommad').click(function(){
     $('#nommad').animate();
-    console.log('a message should appear...');
-    $('#button-msg').show("slow");
-    console.log('redirecting...');
+    console.log("a message should appear...");
+    $('#button-msg').show('slow');
+    console.log("redirecting...");
     setTimeout(function () {
       window.location.href = "https://nommad-app.firebaseapp.com/";
     }, 2000);
